f2697149c3e228d3d76d505dbd01021e7ae4e662
Maximilian Kirschner <kirschner@fzi.de>
2025-02-26 15:24:55 +0100

    Reduced number of performance counter in the ibex

--- rtl/vproc_top.sv
+++ rtl/vproc_top.sv
@@ -126,7 +126,7 @@ module vproc_top import vproc_pkg::*; #(
 
     ibex_xif_top #(
         .RegFile                ( RegFile                            ),
-        .MHPMCounterNum         ( 10                                 ),
+        .MHPMCounterNum         ( 2                                  ),
         .RV32M                  ( ibex_xif_pkg::RV32MFast                ),
         .RV32B                  ( ibex_xif_pkg::RV32BNone                ),
         .ExternalCSRs           ( VECT_CSR_CNT                       ),
